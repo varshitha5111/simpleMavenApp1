f9e42aec83e9c25cfd99c2ad06382621375aba8a
varshitha5111 <varshithabkalmath@gmail.com>
2025-05-07 23:45:27 +0530

    first commit

---
+++ src/main/java/com/example/App2.java
@@ -0,0 +1,18 @@
+package com.example;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App2 
+{
+    public static void main( String[] args )
+    {
+    	Scanner s = new Scanner(System.in);
+    	System.out.println("give a ");
+    	int a = s.nextInt();
+    	int b=4;
+    	int sum=a+b;
+        System.out.println( "sum = "+sum);
+    }
+}
